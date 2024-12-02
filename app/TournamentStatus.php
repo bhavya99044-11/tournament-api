f0b2614e722dc4e5606324ce8b2bec0a67f935ec
Php <bhavya.jain@mindinventory.com>
2024-12-02 19:19:53 +0530

    new data added

--- app/TournamentStatus.php
+++ app/TournamentStatus.php
@@ -4,11 +4,29 @@ namespace App;
 
 enum TournamentStatus:string
 {
-      case Pending = 'pending';
+    case Pending = 'pending';
 
     case Active = 'active';
 
     case Inactive = 'inactive';
 
     case Rejected = 'rejected';
+
 }
+
+// $(document).on('click','.pagination a', function(e){
+//     e.preventDefault();
+//     var page = $(this).attr('href').split('page=')[1];
+//     var sort = null || $(this).attr('href').split('sort=')['sort'];
+//     var dir = null || $(this).attr('href').split('dir=')['dir'];
+//     getPage(sort, dir, page);
+// });
+
+// function getPage(sort, dir, page){
+//     $.ajax({
+//         url: '/admin/user/list?sort=' + sort + '&dir=' + dir + '&page=' + page
+//     }).done(function(data){
+//         $('.ajax').html(data);
+//         $("html, body").animate({ scrollTop: 0 }, 200);
+//     });
+// }
